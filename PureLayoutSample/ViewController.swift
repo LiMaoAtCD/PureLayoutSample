f84889dd833715ee1383f9d3ebac489a08fe413a
liamscofield <limitscofield@gmail.com>
2016-01-17 13:25:27 +0800

    Initial Commit

---
+++ PureLayoutSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PureLayoutSample
+//
+//  Created by AlienLi on 16/1/17.
+//  Copyright © 2016年 MarcoLi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
